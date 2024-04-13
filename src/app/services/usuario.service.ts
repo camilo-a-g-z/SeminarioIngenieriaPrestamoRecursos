afe1c1a1ea6eca3ac84a99522bc194540bde4e95
adrianadiazv <nicolasdiazv1102@gmail.com>
2024-04-13 10:08:27 -0500

    se arreglo la estructura de carpetas 'modulos'

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Firestore, collection, addDoc } from '@angular/fire/firestore';
-import { Usuario } from '../modelo/usuario.model';
+import { Usuario } from '../../interfaces/usuario.model';
 
 @Injectable({
   providedIn: 'root'
